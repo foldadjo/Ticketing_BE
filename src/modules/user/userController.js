b4b58a76c71fc05166bdc1e3b6a24d99d2579a92
unknown <adibmaknun16@gmail.com>
2022-05-15 11:52:42 +0700

    revisi

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -24,6 +24,7 @@ module.exports = {
         {
           name: `${data[0].firstName} ${data[0].lastName}`,
           image: `${data[0].image}`,
+          role: `${data[0].role}`,
         },
       ];
 
